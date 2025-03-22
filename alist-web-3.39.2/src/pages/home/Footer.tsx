7370df7119425f0dc76f4b9f259c4ed83bf93524
ASLant <77436463+Y-ASLant@users.noreply.github.com>
2025-03-22 23:22:17 +0800

    仓库初始化

---
+++ alist-web-3.39.2/src/pages/home/Footer.tsx
@@ -0,0 +1,11 @@
+import { VStack } from "@hope-ui/solid"
+
+export const Footer = () => {
+  return (
+    <VStack class="footer" w="$full" py="$4">
+      {/* 所有内容已被隐藏 */}
+    </VStack>
+  )
+}
+
+export default Footer
---
+++ alist-web-3.39.2/src/types/task.ts
@@ -0,0 +1,10 @@
+export interface TaskInfo {
+  id: string
+  name: string
+  creator: string
+  creator_role: number
+  state: number
+  status: string
+  progress: number
+  error: string
+}
